3f62866f2dae11c52fe441be11dd2da164560a67
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-10-06 08:47:59 +0200

    Create ChatMessage model class

---
+++ backend/Models/Entities/ChatMessage.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Entities;
+
+public class ChatMessage
+{
+    public long Id { get; set; }
+    [Required]
+    public Guid UserId { get; set; }
+    public long SessionId { get; set; }
+    [Required]
+    public string Sender { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}

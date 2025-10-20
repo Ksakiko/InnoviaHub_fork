98136036be322255b289c526d20996fbfb54a190
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-10-20 11:04:06 +0200

    Set up api urls

--- frontend/src/app/core/app-config.service.ts
+++ frontend/src/app/core/app-config.service.ts
@@ -4,9 +4,20 @@ import { Injectable } from '@angular/core';
 export class AppConfigService {
   readonly apiUrl = window.__env?.NG_APP_API_URL || 'http://localhost:5184';
 
-  readonly hubUrl = window.__env?.NG_APP_HUB_URL || 'ws://localhost:5184/hubs/bookings';
+  readonly hubUrl =
+    window.__env?.NG_APP_HUB_URL || 'ws://localhost:5184/hubs/bookings';
 
-  readonly loginRedirectUrl = window.__env?.NG_APP_LOGIN_REDIRECT_URL || 'http://localhost:4200/profil';
+  readonly iotApiUrl =
+    window.__env?.NG_APP_IOT_API_URL || 'http://localhost:5101';
 
-  readonly logoutRedirectUrl = window.__env?.NG_APP_LOGOUT_REDIRECT_URL || 'http://localhost:4200/logga-in';
+  readonly telemetryHubUrl =
+    window.__env?.NG_APP_TELEMETRY_HUB_URL ||
+    'ws://localhost:5103/hub/telemetry';
+
+  readonly loginRedirectUrl =
+    window.__env?.NG_APP_LOGIN_REDIRECT_URL || 'http://localhost:4200/profil';
+
+  readonly logoutRedirectUrl =
+    window.__env?.NG_APP_LOGOUT_REDIRECT_URL ||
+    'http://localhost:4200/logga-in';
 }

6f2980d68a7d81a6251a1f4559ffa10611baeee2
Sakiko Kawahara <sakikokawahar@gmail.com>
2025-10-29 14:10:02 +0100

    Build

---
+++ frontend/dist/frontend/browser/assets/env.js
@@ -0,0 +1,8 @@
+window.__env = {
+  NG_APP_API_URL: 'http://localhost:5184',
+  NG_APP_HUB_URL: 'ws://localhost:5184/hubs/bookings',
+  NG_APP_IOT_API_URL: 'http://localhost:5101',
+  NG_APP_TELEMETRY_HUB_URL: 'ws://localhost:5103/hub/telemetry',
+  NG_APP_LOGIN_REDIRECT_URL: 'http://localhost:4200/profil',
+  NG_APP_LOGOUT_REDIRECT_URL: 'http://localhost:4200/logga-in'
+};

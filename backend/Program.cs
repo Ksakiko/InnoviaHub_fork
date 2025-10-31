6750b0a988f475d0bcff609f7aed3d48fe098f51
Ksakiko <118182699+Ksakiko@users.noreply.github.com>
2025-10-31 08:32:29 +0100

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddHttpClient("openai", client =>
 });
 
 // För att använda inMemory-databas, sätt useInMemory till true
-var useInMemory = true;
+var useInMemory = false;
 
 if (useInMemory)
 {
@@ -103,5 +103,6 @@ app.MapControllers();
 app.MapHub<BookingHub>("/hubs/bookings");
 
 app.Run();
+
 
 
